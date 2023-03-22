afbe47945a2481483db86f6e65f95736350cdccf
Matheus.BAlves <matheus.balves@outlook.com>
2023-03-22 13:30:30 -0300

    First commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px 10px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAF0;
+    color: #FFF;
+    font-size: 24px;
+    font-weight: bold;
+    flex: 1;
+    text-align: center;
+    width: 25%;
+
+    &:hover {
+        cursor: pointer;
+        opacity: 0.6;
+    }
+`
